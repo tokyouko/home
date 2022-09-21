48d8041d5a8f718ce804b4701cd284c0df46e9a8
tokyouko <243085942@qq.com>
2022-09-21 21:34:00 +0800

    项目初始化

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes: any = [
+    { path: '/', component: () => import('@/views/index.vue') },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router;
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <header>hello , welcome to my little house</header>
+        <main></main>
+        <footer></footer>
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
+    
+<style>
+.container {
+    background-color: #fff;
+}
+</style>
+    
